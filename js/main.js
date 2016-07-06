692e12b602bee98fe3d68c2873cba2782a37c196
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-06 18:05:29 -0400

    Finished Exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+/*==============jQuery File=============*/
 
 
+$(document).on("ready", function() {
+    var button = $('.switch > button');
+
+    function changeText() {
+      if (button.hasClass('on')) {
+        $(".status").text("It's so bright in here!");
+      } else {
+        $(".status").text("Hey, who turned off the lights?");
+      }
+    }
+
+    function onOff() {
+      $("body").toggleClass("light dark");
+    }
+
+    function switchOnOff() {
+      button.toggleClass("on off");
+    }
+
+    function buttonClick() {
+      onOff();
+      switchOnOff();
+      changeText();
+    };
+
+    button.on('click', buttonClick);
+});
